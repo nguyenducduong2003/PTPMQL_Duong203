25cdd342390bf459540524b2bf3d490949fdad60
nguyenducduong2003 <duongblue20032003@gmail.com>
2025-01-17 22:55:19 +0700

    BTH_HTML_AND_W3CSS

--- DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d8e44887b0732882e7ea5009750ae8c0028a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+066ca2de5e87449ccc48040b261ee6465fc03e75")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
